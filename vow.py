8b2b7d3d10e5434ee492e479525c541a73489b76
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-01-07 10:04:39 +0000

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,10 @@
+c= input()
+if c.isalpha():
+
+  if c=='A' or c=='a' or c=='E' or c=='e' or c=='I'or c=='i' or c=='O' or c=='o' or c=='U' or c=='u':
+      print("Vowel")
+  else:
+     print("Consonant")
+else:
+  print("invalid")
+     

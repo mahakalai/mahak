817c93cbecf0364af3ae9e4f5fd54ea232f6c2eb
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-01-07 10:00:38 +0000

    Create con.py

---
+++ con.py
@@ -0,0 +1,12 @@
+
+c= input()
+if c.isalpha():
+
+  if ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u':
+      print("Vowel")
+  else:
+     print("Consonant")
+else:
+  print("invalid")
+     
+     

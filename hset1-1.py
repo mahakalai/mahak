72d307e1ddc40d92fbbdab45298bd929fa080d3e
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-05-13 14:00:03 +0530

    Update hset1-1.py

--- hset1-1.py
+++ hset1-1.py
@@ -4,5 +4,8 @@ l1=[]
 for i in range(0,len(l)):
 	if l.count(l[i])>1 :
 		l1.append(l[i])
-l1= list(dict.fromkeys(l1))		
-print(*l1)
+if len(l1)!=0:
+	l1= list(dict.fromkeys(l1))
+	print(*l1)
+else:
+	print("unique")

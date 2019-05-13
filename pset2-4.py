13f6211bd85e432b53e0609e8252668e9f7cf2ed
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-05-13 12:05:44 +0530

    Create pset2-4.py

---
+++ pset2-4.py
@@ -0,0 +1,9 @@
+n=int(input())
+s=input()
+l=list(s)
+for i in range(0,len(s)):
+	m=s[i].lower()
+	if m=="a" or m=="e" or m=="i" or m=="o" or m=="u":
+		l.remove(m)
+for i in reversed(l):
+	print(i,end="")

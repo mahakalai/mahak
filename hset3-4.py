e279aefe0c02e55676e5bd4cf1feb2d80b7ca983
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-05-14 09:48:07 +0530

    Create hset3-4.py

---
+++ hset3-4.py
@@ -0,0 +1,16 @@
+n,k=map(int,input().split())
+l=[int(x) for x in input().split()]
+i=0
+c=0
+while i<len(l):
+	j=i+1
+	while j<len(l):
+		s=l[i]+l[j]
+		if k==s:
+			print("yes")
+			c=c+1
+			break
+		j=j+1
+	i=i+1
+if c==0:
+	print("no")

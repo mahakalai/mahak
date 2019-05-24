034db7be2f00b6a650d67796dd25d763aeffec4e
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-05-24 15:03:10 +0530

    Update hun69.py

--- hun69.py
+++ hun69.py
@@ -7,7 +7,7 @@ l=len(s)
 i1=s.index(".")
 s2=s[i1::]
 s3=s[i+1:i1]
-if c1==1 and c2==1 and l>=3 and s2==".com" and len(s3)=="gmail":
+if c1==1 and c2==1 and l>=3 and s2==".com" and s3=="gmail":
 	print("YES")
 else:
 	print("NO")

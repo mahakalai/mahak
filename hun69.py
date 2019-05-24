ab633f9e1ee49828af982ae3b3e5e477e5f509d6
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-05-24 15:00:30 +0530

    Update hun69.py

--- hun69.py
+++ hun69.py
@@ -5,8 +5,9 @@ i=s.index("@")
 s1=s[:i]
 l=len(s)
 i1=s.index(".")
-s1=s[i1::]
-if c1==1 and c2==1 and l>=3 and s1==".com":
+s2=s[i1::]
+s3=s[i+1:i1]
+if c1==1 and c2==1 and l>=3 and s2==".com" and len(s3)=="gmail":
 	print("YES")
 else:
 	print("NO")

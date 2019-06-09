893b38cc2965af6039054fe60eaf8b0a8c00a3a6
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-06-09 16:45:53 +0530

    Update 16hun.py

--- 16hun.py
+++ 16hun.py
@@ -1,3 +1,4 @@
+import operator
 n,k=map(int,input().split())
 l=[int(x) for x in input().split()]
 l1={}
@@ -5,6 +6,7 @@ for i in range(0,len(l)):
 	d=abs(l[i]-k)
 	if d!=0:
 		l1.update({l[i]:d})
-l2=sorted(l1)
-l3=l2[:3]
+sorted_x = sorted(l1.items(), key=operator.itemgetter(1))
+l2=sorted_x[:3]
+l3=[i[0] for i in l2]
 print(*l3)

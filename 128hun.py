9346b45b9c54bd486f0748f34a3398d9f3784e36
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-06-18 16:23:04 +0530

    Update 128hun.py

--- 128hun.py
+++ 128hun.py
@@ -1,8 +1,13 @@
 s=input()
-if s==s[::-1]:
-	print(s)
+s2=s.lower()
+l=[]
+if s2==s2[::-1]:
+	l.append(s2)
 for i in range(0,len(s)-1):
 	for j in range(i+1,len(s)):
-		s1=s[i:j]
+		s1=s2[i:j]
 		if s1==s1[::-1] and len(s1)>1:
-			print(s1)
+			l.append(s1)
+l1=sorted(l)
+for i in l1:
+	print(i)

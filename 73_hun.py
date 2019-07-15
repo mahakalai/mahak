613be9df7743ef59b1f0e07b7df987d29bb23ec7
mahakalai <46440625+mahakalai@users.noreply.github.com>
2019-07-15 21:58:32 +0530

    Update 73_hun.py

--- 73_hun.py
+++ 73_hun.py
@@ -1,9 +1,12 @@
 s=input()
 s1=input()
 l=[]
-for i in range(0,len(s)):
-	for j in range(i+1,len(s)):
-		k=s1[i:j]
-		if k in s:
-			l.append(k)
-print(max(l, key=len))
+if s==s1:
+	print(s)
+else:
+	for i in range(0,len(s)):
+		for j in range(i+1,len(s)):
+			k=s1[i:j]
+			if k in s:
+				l.append(k)
+	print(max(l, key=len))
